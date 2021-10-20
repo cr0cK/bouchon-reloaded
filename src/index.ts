788fc43d592b6b5a64da92926a7b23477cafca2f
Alexis Mineaud <amineaud@tenable.com>
2021-10-20 15:33:30 +0200

    Expose logger.

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export { bouchonServer } from '@libs/bouchon/server'
 export { bouchonRouter } from '@libs/router'
 export { parseData } from '@libs/bouchon/helpers'
 export { getDefaultResponseStatusCode } from '@libs/router/helpers'
+export { newLogger } from '@libs/logger'
 
 export type { Action } from '@libs/types'
